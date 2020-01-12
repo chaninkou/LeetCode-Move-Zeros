5edc7bbbeef4676ea4cb97c877e1475b48b2941e
chaninkou <michaelkoulove@yahoo.com>
2020-01-12 00:33:53 -0800

    Great review on in-place array, using a technique to put elements first

--- src/zeroEnd/MoveZeroesToEndFunction.java
+++ src/zeroEnd/MoveZeroesToEndFunction.java
@@ -4,18 +4,23 @@ import java.util.Arrays;
 
 public class MoveZeroesToEndFunction {
     public void moveZeroes(int[] nums) {
+    	// We want to put everything on the left first
         int index = 0;
         
         // Insert the non zero number first
         for(int i = 0; i < nums.length; i++){
             if(nums[i] != 0){
-                nums[index++] = nums[i];
+                nums[index] = nums[i];
+                
+                // Increase to next element index
+                index++;
             }
         }
         
-        // Insert the zeros now
+        // Insert the zeros at the end now starting off from index
         while(index < nums.length){
-            nums[index++] = 0;
+            nums[index] = 0;
+            index++;
         }
         
         System.out.println("Solution: " + Arrays.toString(nums));
